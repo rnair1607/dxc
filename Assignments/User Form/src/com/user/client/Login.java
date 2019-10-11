0cbffba9c75911f87df7edaa2c7b6b46da5c0eb0
Rahul Nair <51226640+rnair1607@users.noreply.github.com>
2019-10-11 09:29:53 +0530

    Add files via upload

---
+++ Assignments/User Form/src/com/user/client/Login.java
@@ -0,0 +1,68 @@
+package com.user.client;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.user.dao.LoginDAO;
+import com.user.dao.LoginDAOImpl;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+	
+		
+		LoginDAO loginDAO = new LoginDAOImpl();
+		HttpSession session = request.getSession();
+		session.setAttribute("username", username);
+		session.setAttribute("password", password);
+		
+		if(loginDAO.validate(username, password)) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Welcome.html");
+			dispatcher.forward(request, response);
+		}
+		else {
+			response.sendRedirect("Login.html");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ Assignments/User Form/src/com/user/dao/LoginDAO.java
@@ -0,0 +1,7 @@
+package com.user.dao;
+import com.user.dao.*;
+public interface LoginDAO 
+{
+	public boolean validate(String userName, String password);
+
+}
